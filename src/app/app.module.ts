3c58468f9b8a274a5432273f462ff070461da5c2
Tarun Bhartiya <tarun.bhartiya@aig.com>
2017-04-10 20:51:31 +0530

    First comit completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SimpleTinyComponent } from './simple-tiny/simple-tiny.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleTinyComponent
   ],
   imports: [
     BrowserModule,
